095bfdb907cb1ec591496499627e1d1082c71457
Maxence Court <maxencecourt1@gmail.com>
2019-03-20 11:02:23 +0100

    Second Commit

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-fsfdsfds
+const chalk = require('chalk');
+
+
+console.log(chalk.blue('Mick'));
+console.log(chalk.green('Ben1'));
+console.log(chalk.yellow('Dimi'));
+console.log(chalk.magenta('Ben2'));
